9cec91dd82313620537ed1f1aea91307099e8f94
Mohammed H. Mahmood <mohammed.hamidf18@komar.edu.iq>
2023-09-13 16:16:12 +0300

    added save place functionality

--- lib/screens/new_place_screen.dart
+++ lib/screens/new_place_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:favorite_places/providers/place_provider.dart';
+import 'package:favorite_places/providers/user_places.dart';
 
 class NewPlaceScreen extends ConsumerStatefulWidget {
   const NewPlaceScreen({super.key});
@@ -13,29 +13,45 @@ class _NewPlaceScreenState extends ConsumerState<NewPlaceScreen> {
   final _titleController = TextEditingController();
 
   @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
+  void _savePlace() {
+    if (_titleController.text.isEmpty) {
+      return;
+    }
+    ref.read(placeProvider.notifier).addPlace(_titleController.text);
+    Navigator.of(context).pop();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final provider = ref.read(placeProvider.notifier);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Add New Place"),
       ),
       body: Container(
         padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _titleController,
-              decoration: const InputDecoration(
-                labelText: "Title",
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                controller: _titleController,
+                decoration: const InputDecoration(
+                  labelText: "Title",
+                ),
+                style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
               ),
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton.icon(
-              onPressed: () => provider.addPlace(_titleController.text),
-              icon: const Icon(Icons.add),
-              label: const Text("Add Place"),
-            ),
-          ],
+              const SizedBox(height: 16.0),
+              ElevatedButton.icon(
+                onPressed: _savePlace,
+                icon: const Icon(Icons.add),
+                label: const Text("Add Place"),
+              ),
+            ],
+          ),
         ),
       ),
     );
