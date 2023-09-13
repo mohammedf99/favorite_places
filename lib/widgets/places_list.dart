62994851366813bae81430673055bff5da6b3446
Mohammed H. Mahmood <mohammed.hamidf18@komar.edu.iq>
2023-09-13 16:14:32 +0300

    added function to view place detail screen

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -1,22 +1,47 @@
 import 'package:flutter/material.dart';
 
 import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/screens/place_details_screen.dart';
 
 class PlacesList extends StatelessWidget {
   const PlacesList({super.key, required this.places});
 
   final List<Place> places;
 
+  void _openPlaceDetails(BuildContext context, Place place) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => PlaceDetailsScreen(
+          place: place,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (places.isEmpty) {
+      return Center(
+        child: Text(
+          "No places yet, start adding some!",
+          style: Theme.of(context)
+              .textTheme
+              .bodyLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
+      );
+    }
     return ListView.builder(
       itemCount: places.length,
       itemBuilder: (ctx, index) {
         return ListTile(
           title: Text(
             places[index].title,
-            style: Theme.of(context).textTheme.titleLarge,
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
           ),
+          onTap: () => _openPlaceDetails(context, places[index]),
         );
       },
     );
