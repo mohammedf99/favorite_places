38783a5b6500727e85f6b59ae39c20bf6f75156d
Mohammed H. Mahmood <mohammed.hamidf18@komar.edu.iq>
2023-09-13 16:13:18 +0300

    added ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
-import 'package:favorite_places/screens/places_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:favorite_places/theme.dart';
+import 'package:favorite_places/screens/places_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
