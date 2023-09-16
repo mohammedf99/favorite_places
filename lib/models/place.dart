45135035eb19cacb378d6e3541a08661b520f842
Mohammed H. Mahmood <mohammed.hamidf18@komar.edu.iq>
2023-09-16 12:12:37 +0300

    added image file to the model

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,12 +1,16 @@
 import 'package:uuid/uuid.dart';
+import 'dart:io';
 
 const uuid = Uuid();
 
 class Place {
   final String id;
   final String title;
+  final File image;
+
 
   Place({
     required this.title,
+    required this.image,
   }) : id = uuid.v4();
 }
